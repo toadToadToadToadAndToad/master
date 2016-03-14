5f85f05e880a95bec6b9d878ecc24d243ebec9c6
Tim Dang <timothy.dang@gmail.com>
2016-03-14 08:39:15 -0700

    initial setup

---
+++ test/searchResultsContainer.js
@@ -0,0 +1,20 @@
+/* global searchResultsContainer, describe, it, expect, should */
+
+describe('searchResultsContainer()', function () {
+  'use strict';
+
+  it('exists', function () {
+    expect(searchResultsContainer).to.be.a('function');
+
+  });
+
+  it('does something', function () {
+    expect(true).to.equal(false);
+  });
+
+  it('does something else', function () {
+    expect(true).to.equal(false);
+  });
+
+  // Add more assertions here
+});
