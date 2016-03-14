578bb8674564e2d7be35d8a7b66fc4e6e15274f5
Michael Dziedzic <michaeldzie@gmail.com>
2016-03-14 10:49:27 -0700

    (design) material color change

--- app/styles/material-customizations.js
+++ app/styles/material-customizations.js
@@ -16,7 +16,7 @@ export default {
     accent3Color: Colors.grey500,
     textColor: Colors.darkBlack,
     alternateTextColor: Colors.white,
-    canvasColor: Colors.black,
+    canvasColor: Colors.white,
     borderColor: Colors.grey300,
     disabledColor: ColorManipulator.fade(Colors.darkBlack, 0.3),
     pickerHeaderColor: Colors.cyan500,
