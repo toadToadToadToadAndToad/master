a53322b1e40174f82148ef42fc8f1f3680bf9e80
Michael Dziedzic <michaeldzie@gmail.com>
2016-03-16 10:45:02 -0700

    (refactor) got rid of config/core.js

--- app/config/reducer.js
+++ app/config/reducer.js
@@ -1,48 +1,49 @@
 import { Map, List } from 'immutable';
-import * as coreFuncs from './core';
 
 export default function reducer(state = Map(), action) {
 
   switch (action.type) {
 
+    // set entire collections
+
     case 'SET_STATE':
       return state.merge(action.state);
-
     case 'SET_JOBS':
       return state.set('jobs', List(action.jobs.map((job) => {
         return Map(job);
       })));
-
     case 'SET_EVENTS':
       return state.set('events', List(action.events.map((event) => {
         return Map(event);
       })));
-
     case 'SET_CONTACTS':
-      return coreFuncs.setContacts(state, action.contacts);
+      return state.set('contacts', List(action.contacts));
     case 'SET_USERINFO':
-      return coreFuncs.setUserInfo(state, action.userinfo);
+      return state.set('userInfo', Map(action.userInfo));
+
+    // add individual items to collections
 
     case 'ADD_JOB':
-      return coreFuncs.addJob(state, action.job);
+      return state.update('jobs',
+        jobs => jobs.push(Map(action.job)));
     case 'ADD_EVENT':
-      return coreFuncs.addEvent(state, action.event);
+      return state.update('events',
+        events => events.push(Map(action.event)));
     case 'ADD_CONTACT':
-      return coreFuncs.addContact(state, action.contact);
+      return state.update('contacts',
+        contacts => contacts.push(Map(action.contact)));
+
+    // delete individual items from collections
 
     case 'DELETE_JOB':
-      return coreFuncs.deleteJob(state, action.job);
     case 'DELETE_EVENT':
-      return coreFuncs.deleteEvent(state, action.event);
     case 'DELETE_CONTACT':
-      return coreFuncs.deleteContact(state, action.contact);
+
+    // upate individual item in a collection
 
     case 'UPDATE_JOB':
-      return coreFuncs.updateJob(state, action.job);
     case 'UPDATE_EVENT':
-      return coreFuncs.updateEvent(state, action.event);
     case 'UPDATE_CONTACT':
-      return coreFuncs.updateContact(state, action.contact);
 
     default:
       return state;
