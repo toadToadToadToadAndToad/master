587f767732eba4da3b7fb0d6dba2292e41e5fe9b
Aimee Rosato <aimee.rosato@gmail.com>
2016-03-17 15:13:09 -0400

    (feat) Database to Koa work

--- database/rethinkdb.js
+++ database/rethinkdb.js
@@ -1,11 +1,32 @@
 /*TODO: Add to Koa server */
 
-//Import Rethink
+import thinky from './thinkylocal';
+
+// Import Rethink
 import r from 'rethinkdb';
 import config from 'config.js';
 
-let connection = null;
-r.connect({ host: 'localhost', port: 28015, db: 'test', }, (err, conn) => {
-  if (err) throw err;
-  connection = conn;
-});
+// Create a rethinkdb connection - they save it in req._rdbConn
+app.use(createConnection);
+
+app.use(closeConnection);
+
+
+function* createConnection(next) {
+  try {
+    const conn = yield r.connect(config.rethinkdb);
+    this._rdbConn = conn;
+  }
+  catch(err) {
+    this.status = 500;
+    this.body = err.message || 'Could not connect to DB';
+  }
+  yield next;
+}
+
+/*
+ * Close the RethinkDB connection
+ */
+function* closeConnection(next) {
+    this._rdbConn.close();
+}
