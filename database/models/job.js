60e69b47b491a94cb977eeaa89962131c9efbe4f
Aimee Rosato <aimee.rosato@gmail.com>
2016-03-17 14:04:38 -0400

    (feat) Modified schema to match page

--- database/models/job.js
+++ database/models/job.js
@@ -6,6 +6,13 @@ let Job = thinky.createModel('Job', {
   title: type.string(),
   company: type.string(),
   location: type.string(),
+  type: type.string.enum(['fulltime', 'partime', 'contract']),
+  description: type.string().max(200),
+  url: type.string(),
+  contact: {
+    email: type.string(),
+    phone: type.number(),
+  },
 });
 
 export default Job;
