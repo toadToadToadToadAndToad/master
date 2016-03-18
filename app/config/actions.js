c9fe2508d6f479ab060bec5ba37334a4a112ee57
Michael Dziedzic <michaeldzie@gmail.com>
2016-03-18 16:57:31 -0700

    (style) added semicolongs

--- app/config/actions.js
+++ app/config/actions.js
@@ -27,59 +27,58 @@ export const UPDATE_CONTACT = 'UPDATE_CONTACT';
 export const SET_USERINFO = 'SET_USERINFO';
 
 
-
 /*
  * action creators
  */
 
 // state
 export function setState(state) {
-  return { type: SET_STATE, state }
+  return { type: SET_STATE, state };
 }
 
 // jobs
 export function setJobs(jobs) {
-  return { type: SET_JOBS, jobs }
+  return { type: SET_JOBS, jobs };
 }
 export function addJob(job) {
-  return { type: ADD_JOB, job }
+  return { type: ADD_JOB, job };
 }
 export function deleteJob(jobID) {
-  return { type: DELETE_JOB, jobID }
+  return { type: DELETE_JOB, jobID };
 }
 export function updateJob(job) {
-  return { type: UPDATE_JOB, job }
+  return { type: UPDATE_JOB, job };
 }
 
 // events
 export function setEvents(events) {
-  return { type: SET_EVENTS, events }
+  return { type: SET_EVENTS, events };
 }
 export function addEvent(event) {
-  return { type: ADD_EVENT, event }
+  return { type: ADD_EVENT, event };
 }
 export function deleteEvent(eventID) {
-  return { type: DELETE_EVENT, eventID }
+  return { type: DELETE_EVENT, eventID };
 }
 export function udpateEvent(event) {
-  return { type: UPDATE_EVENT, event }
+  return { type: UPDATE_EVENT, event };
 }
 
 // contacts
 export function setContacts(contacts) {
-  return { type: SET_CONTACTS, contacts }
+  return { type: SET_CONTACTS, contacts };
 }
 export function addContact(contact) {
-  return { type: ADD_CONTACT, contact }
+  return { type: ADD_CONTACT, contact };
 }
 export function deleteContact(contactID) {
-  return { type: DELETE_CONTACT, contactID }
+  return { type: DELETE_CONTACT, contactID };
 }
 export function udpateContact(contact) {
-  return { type: UPDATE_CONTACT, contact }
+  return { type: UPDATE_CONTACT, contact };
 }
 
 // userInfo
 export function setUserInfo(unserInfo) {
-  return { type: SET_USERINFO, unserInfo }
+  return { type: SET_USERINFO, unserInfo };
 }
