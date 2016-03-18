c5775b2566a43bff0605b53e16404b68c1c34325
Tim Dang <timothy.dang@gmail.com>
2016-03-18 15:23:56 -0700

    (chore) deleted and undeleted webpack config

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+module.exports = {
+  entry: [
+    'babel-polyfill',
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './app/index.js'
+  ],
+  output: {
+    path: __dirname + '/dist',
+    filename: "index_bundle.js"
+  },
+  devtool: "eval",
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "react-hot!babel-loader"
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader" }
+    ]
+  },
+  devServer: {
+    contentBase: '/dist',
+    hot: true
+  },
+  plugins: [
+    HTMLWebpackPluginConfig,
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
