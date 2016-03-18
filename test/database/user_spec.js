0fe0a03c2035645351f62e011235c285ae1cce89
Aimee Rosato <aimee.rosato@gmail.com>
2016-03-18 16:08:38 -0400

    (feat) Testing started / modified package.json so could test es6 code

---
+++ test/database/user_spec.js
@@ -0,0 +1,55 @@
+'use strict';
+
+import r from 'rethinkdb';
+import chai from 'chai';
+import User from '../../database/models/user';
+import Job from '../../database/models/job';
+import thinky from '../../database/thinkylocal.js';
+import userController from '../../server/controllers/user';
+
+let expect = chai.expect;
+
+describe('User Controller Methods Testing', () => {
+
+  // Did NOT end up needed these for tests below, but may need for future db tests
+  // beforeEach((done) => {
+  //   let connection = null;
+  //   r.connect({ host: 'localhost', port: 28015, db: 'test', }, (err, conn) => {
+  //     if (err) throw err;
+  //     connection = conn;
+  //   });
+  //   done();
+  // });
+
+  // afterEach(() => {
+  //   r.dbDrop('test');
+  // });
+
+  it('should have all proper methods', () => {
+    expect(addUser).to.be.a('function');
+    expect(deleteUser).to.be.a('function');
+  });
+
+//   it('should store passed properties when instantiated', () => {
+//     const username = 'bsmith';
+//     const email = 'bsmith@io.io';
+//     const password = 'fjalskdjf';
+//     const user = new User ({ username, email, password });
+//     expect(user.username).to.equal('bsmith');
+//     expect(user.email).to.equal('bsmith@io.io');
+//     expect(user.password).to.equal('fjalskdjf');
+//   });
+
+//   it('should create a job', () => {
+//     const job = new Job({});
+//     expect(job).to.be.a('object');
+//   });
+
+//   it('should store passed properties when instantiated', () => {
+//     const title = 'Junior Dev';
+//     const company = 'Github Inc.';
+//     const job = new Job({ title, company, location });
+//     expect(job.title).to.equal('Junior Dev');
+//     expect(job.company).to.equal('Github Inc.');
+//   });
+});
