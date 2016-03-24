c327c3f07a0269ae5206132f036ac83b720a4012
Tim Dang <timothy.dang@gmail.com>
2016-03-24 10:34:58 -0700

    (fix) beautify

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -3,35 +3,6 @@
 const User = require('../../database/models/user');
 const parse = require('co-body');
 const http = require('http');
-const r = require('rethinkdb');
-const jwt = require('koa-jwt');
-// const currentUser = require('./auth');
-
-module.exports.addUser = function* (next) {
-  // this.type = 'application/json';
-  // try {
-  //   let data = yield parse(this);
-  //   // TODO: Hash password here: data.password = hashhhhhh
-  //   // Create new instance of 'User' model
-  //   const user = new User(data);
-
-  //   // Check to see if user already exists
-  //   const existing = yield User.filter({ email: user.email }).limit(1).run();
-
-  //   if (existing.length !== 0) {
-  //     // throw and error
-  //     throw new Error('Email already taken');
-  //   }
-
-  //   // Otherwise, save the user to the table
-  //   yield user.save();
-  // } catch (e) {
-  //   this.status = 500;
-  //   this.body = e.message || http.STATUS_CODES[this.status];
-  // }
-
-  // yield next;
-};
 
 module.exports.deleteUser = function* (next) {
   try {
@@ -42,7 +13,6 @@ module.exports.deleteUser = function* (next) {
     const result = yield User.get(user.id)
                              .delete().run();
     console.log('User deleted sucessfully.');
-    // this.body = '';
   } catch (e) {
     this.status = 500;
     this.body = e.message || http.STATUS_CODES[this.status];
