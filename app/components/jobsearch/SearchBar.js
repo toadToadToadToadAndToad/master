f8b4977e980e413043d0bcb8631d37755f9f18f3
Michael Dziedzic <michaeldzie@gmail.com>
2016-03-26 07:55:22 -0700

    (refactor) SearchBar and JobSearchContainer

--- app/components/jobsearch/SearchBar.js
+++ app/components/jobsearch/SearchBar.js
@@ -1,50 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import TextField from 'material-ui/lib/text-field';
 
-class SearchBarComponent extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      keywords: '',
-      location: '',
-    };
-    this.handleKeyChange = this.handleKeyChange.bind(this);
-    this.handleLocationChange = this.handleLocationChange.bind(this);
-    this.handleSearch = this.handleSearch.bind(this);
-  }
-  handleKeyChange(e) {
-    this.setState({ keywords: e.target.value });
-  }
-  handleLocationChange(e) {
-    this.setState({ location: e.target.value });
-  }
-  handleSearch(event) {
-    event.preventDefault();
-    this.props.onHandleSearch(this.state.keywords, this.state.location);
-    this.setState({ keywords: '', location: '' });
-  }
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.handleSearch}>
-          <TextField
-            value={this.state.keywords}
-            onChange={this.handleKeyChange}
-            onEnterKeyDown={this.handleSearch}
-            hintText="Keywords"
-          />&nbsp;&nbsp;&nbsp;&nbsp;
-          <TextField
-            value={this.state.location}
-            onChange={this.handleLocationChange}
-            onEnterKeyDown={this.handleSearch}
-            hintText="Location"
-          />
-        </form>
-      </div>
-    );
-  }
+function SearchBar(props) {
+  return (
+    <div>
+      <form onSubmit={props.onHandleSearch}>
+        <TextField
+          value={props.keywords}
+          onChange={props.onHandleKeyChange}
+          onEnterKeyDown={props.onHandleSearch}
+          hintText="Keywords"
+        />&nbsp;&nbsp;&nbsp;&nbsp;
+        <TextField
+          value={props.location}
+          onChange={props.onHandleLocationChange}
+          onEnterKeyDown={props.onHandleSearch}
+          hintText="Location"
+        />
+      </form>
+    </div>
+  );
 }
 
-SearchBarComponent.propTypes = { onHandleSearch: React.PropTypes.func };
-export default SearchBarComponent;
+SearchBar.propTypes = {
+  keywords: PropTypes.string,
+  location: PropTypes.string,
+  onHandleSearch: PropTypes.func,
+  onHandleLocationChange: PropTypes.func,
+  onHandleKeyChange: PropTypes.func,
+};
+
+export default SearchBar;
--- app/containers/JobSearchContainer.js
+++ app/containers/JobSearchContainer.js
@@ -1,9 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
 import axios from 'axios';
-import SearchBarComponent from '../components/jobsearch/SearchBar';
-// import SiteSelectionComponent from '../components/jobsearch/SiteSelect';
+import SearchBar from '../components/jobsearch/SearchBar';
 import ResultsViewComponent from '../components/jobsearch/ResultsView';
 import RaisedButton from 'material-ui/lib/raised-button';
 import PageHeader from 'react-bootstrap/lib/PageHeader';
@@ -12,16 +11,20 @@ import { addJob } from '../config/actions';
 class JobSearchContainer extends Component {
   constructor() {
     super();
+    this.handleKeyChange = this.handleKeyChange.bind(this);
+    this.handleLocationChange = this.handleLocationChange.bind(this);
+    this.handleSearchSubmit = this.handleSearchSubmit.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+    this.handleRowClick = this.handleRowClick.bind(this);
+
     this.state = {
       data: [],
+      keywords: '',
+      location: '',
     };
-    this.handleSearchSubmit = this.handleSearchSubmit.bind(this);
-    this.handleRowClick = this.handleRowClick.bind(this);
   }
 
   componentDidMount() {
-    // The state keeps on getting reset on load. Need to see how to persist a search result.
-    console.log('Mounted data:', this.state.data);
   }
 
   strip(html) {
@@ -32,6 +35,17 @@ class JobSearchContainer extends Component {
     return tmp.textContent || tmp.innerText || "";
   }
 
+  handleKeyChange(e) {
+    this.setState({ keywords: e.target.value });
+  }
+  handleLocationChange(e) {
+    this.setState({ location: e.target.value });
+  }
+  handleSearch(event) {
+    event.preventDefault();
+    this.handleSearchSubmit(this.state.keywords, this.state.location);
+    this.setState({ keywords: '', location: '' });
+  }
   handleSearchSubmit(keyword, location) {
     this.setState({ data: [] });
     // TODO add loading spinner
@@ -76,7 +90,13 @@ class JobSearchContainer extends Component {
     return (
       <div>
         <h2>Job Search</h2>
-        <SearchBarComponent onHandleSearch={this.handleSearchSubmit} />
+        <SearchBar
+          keywords={this.state.keywords}
+          location={this.state.location}
+          onHandleSearch={this.handleSearch}
+          onHandleLocationChange={this.handleLocationChange}
+          onHandleKeyChange={this.handleKeyChange}
+        />
         <br /><br />
         <ResultsViewComponent
           data={this.state.data}
@@ -87,5 +107,8 @@ class JobSearchContainer extends Component {
   }
 }
 
-JobSearchContainer.propTypes = { dispatch: React.PropTypes.func };
+JobSearchContainer.propTypes = {
+  dispatch: PropTypes.func,
+};
+
 export default connect()(JobSearchContainer);
