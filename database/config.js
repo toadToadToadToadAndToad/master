07fa2eb0e17e8f6c6136915e7fe14a454d92d790
Tim Dang <timothy.dang@gmail.com>
2016-03-29 14:27:31 -0700

    (feat) remote rethinkDB

--- database/config.js
+++ database/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   rethinkdb: {
-    host: 'aws-us-east-1-portal.12.dblayer.com',
+    host: 'rethinkdb://aws-us-east-1-portal.12.dblayer.com',
     port: 10419,
     authKey: 'WXDkgpygPhK57JELT22QmbYEw7sq9DnpRuSswcSiF4',
     db: 'jobs',
