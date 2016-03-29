e66209db78c87821420d6d4a7add0296ec98f5be
Tim Dang <timothy.dang@gmail.com>
2016-03-29 15:02:24 -0700

    (feat) remote rethinkDB

--- database/config.js
+++ database/config.js
@@ -1,15 +1,11 @@
 module.exports = {
   rethinkdb: {
     database: 'jobs',
-    discovery: false,
-    timeout: 10,
     authKey: 'WXDkgpygPhK57JELT22QmbYEw7sq9DnpRuSswcSiF4',
-    servers: [{
-      host: 'aws-us-east-1-portal.12.dblayer.com',
-      port: 10419,
-    }],
+    host: 'aws-us-east-1-portal.12.dblayer.com',
+    port: 10419,
     ssl: {
-      rejectUnauthorized: true,
+      ca: publicKey,
     },
   },
   koa: {
