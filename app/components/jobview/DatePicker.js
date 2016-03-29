db1d09deed6face3ebb50a7990e70a1c30919741
Aimee Rosato <aimee.rosato@gmail.com>
2016-03-28 20:30:38 -0400

    (feat) work on reminders

---
+++ app/components/jobview/DatePicker.js
@@ -0,0 +1,13 @@
+// import React from 'react';
+// import { Link } from 'react-router';
+// import DatePicker from 'material-ui/lib/date-picker/date-picker';
+
+// function DatePickerComponenet() {
+//   return (
+//     <div className="delete-job">
+//         <DatePicker hintText="Choose your date" mode="landscape" />
+//     </div>
+//   );
+// }
+
+// export default DatePickerComponenet;
