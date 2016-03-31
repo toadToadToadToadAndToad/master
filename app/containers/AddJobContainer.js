ef330b8f74fce83deff25f5629a5a5e85f366f66
Michael Dziedzic <michaeldzie@gmail.com>
2016-03-30 20:33:27 -0700

    (refactor)(design) add job

--- app/containers/AddJobContainer.js
+++ app/containers/AddJobContainer.js
@@ -1,37 +1,151 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import AddJobComponent from '../components/addjob/addjob';
-
+import { browserHistory } from 'react-router';
+import TextField from 'material-ui/lib/text-field';
+import SelectField from 'material-ui/lib/select-field';
+import MenuItem from 'material-ui/lib/menus/menu-item';
+import FloatingActionButton from 'material-ui/lib/floating-action-button';
+import FlatButton from 'material-ui/lib/flat-button';
+import ContentAdd from 'material-ui/lib/svg-icons/content/add';
+import Dialog from 'material-ui/lib/dialog';
 import { addJob } from '../config/actions';
 
-const handleSubmit = (event, dispatch) => {
-    event.preventDefault();
+class AddJobContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      company: '',
+      title: '',
+      location: '',
+      url: '',
+      type: 1,
+      description: '',
+      open: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleTextInput = this.handleTextInput.bind(this);
+    this.saveJobsToStore = this.saveJobsToStore.bind(this);
+  }
 
-    let children = event.target.children;
-    let job = {
-    title: children.title.value,
-    company: children.company.value,
-    description: children.description.value,
-    type: children.type.value,
-    location: children.location.value,
-    url: children.location.url,
-    how_to_apply: children['how-to-apply'].value
-  };
-  event.target.reset();
-  dispatch(addJob(job));
+  handleChange(event, index, value) {
+    this.setState({ type: value });
+  }
+
+  handleTextInput(event, id) {
+    var update = {};
+    update[event.target.id] = event.target.value;
+    this.setState(update);
+  }
+
+  handleOpen() {
+    this.setState({ open: true });
+  }
+
+  handleClose() {
+    browserHistory.push('/dashboard');
+  }
+
+  saveJobsToStore() {
+    const jobTypes = [
+      'Full Time',
+      'Part Time',
+      'Temporary',
+      'Contract',
+    ];
+
+    const job = {
+      company: this.state.company,
+      title: this.state.title,
+      location: this.state.location,
+      type: jobTypes[this.state.type - 1],
+      url: this.state.url,
+      description: this.state.description,
+    }
+
+    this.props.dispatch(addJob(job));
+    this.handleOpen();
+  }
+
+  render() {
+    const actions = [
+      <FlatButton
+        label="OK"
+        primary
+        onMouseDown={this.handleClose}
+      />,
+    ];
+
+    return (
+      <div>
+        <h1>Add Job</h1>
+        <form
+          className="job-form"
+        >
+          <TextField
+            id="company"
+            hintText="Company"
+            onChange={this.handleTextInput}
+          />&nbsp;&nbsp;
+          <TextField
+            id="title"
+            hintText="Job Title"
+            onChange={this.handleTextInput}
+          /><br />
+          <TextField
+            id="location"
+            hintText="Location"
+            onChange={this.handleTextInput}
+          />&nbsp;&nbsp;
+          <TextField
+            id="url"
+            hintText="URL"
+            onChange={this.handleTextInput}
+          /><br />
+          <SelectField
+            id="type"
+            value={this.state.type}
+            onChange={this.handleChange}
+          >
+            <MenuItem value={1} primaryText="Full Time" />
+            <MenuItem value={2} primaryText="Part Time" />
+            <MenuItem value={3} primaryText="Temporary" />
+            <MenuItem value={4} primaryText="Contract" />
+          </SelectField><br />
+          <TextField
+            id="description"
+            hintText="Description"
+            multiLine
+            rows={3}
+            onChange={this.handleTextInput}
+          />
+        </form>
+        <div
+          className="addJobButton-aj">
+            <FloatingActionButton
+              mini
+              className="button-circle"
+              onMouseDown={this.saveJobsToStore}
+            >
+              <ContentAdd />
+            </FloatingActionButton>
+          <span className="button-circle-text">Add Job</span>
+          <br /><br /><br /><br />
+        </div>
+        <Dialog
+          title="Job Added!"
+          actions={actions}
+          modal
+          open={this.state.open}
+        >
+          The job has been added to your dashboard.
+        </Dialog>
+      </div>
+    );
+  }
 }
 
-let  AddJobContainer = ({dispatch}) => {
-  return (
-      <div>
-        <AddJobComponent
-        handleSubmit={((event) => handleSubmit(event, dispatch))} />
-        <Link to="/dashboard">Back to Dashboard</Link>
-      </div>
-  )
+AddJobContainer.propTypes = {
+  dispatch: PropTypes.func,
 };
 
-AddJobContainer = connect()(AddJobContainer);
-export default AddJobContainer;
+export default connect()(AddJobContainer);
