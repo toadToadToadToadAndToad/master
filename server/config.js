e8abfc4a5849b4f58ad06a2957abbccaea0654b8
Tim Dang <timothy.dang@gmail.com>
2016-04-07 17:38:49 -0700

    Heroku tweaks.

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 const tokens = {
-  GOOGLE_CLIENT_ID: '314239622238-5c85ejracebj8a87m6k3j1ro4vfuek1e.apps.googleusercontent.com',
-  GOOGLE_CLIENT_SECRET: 'ZGKJuhOvnd9sxjvHv-MSlFzY',
+  GOOGLE_CLIENT_ID: process.env.client,
+  GOOGLE_CLIENT_SECRET: process.env.secret,
 };
 module.exports = tokens;
