aa06b95a24aac5fcd33bf66ef8cfb61202487a70
Tim Dang <timothy.dang@gmail.com>
2016-04-07 18:08:16 -0700

    (fix) db access for heroku

--- database/config.js
+++ database/config.js
@@ -4,6 +4,9 @@ module.exports = {
     authKey: 'WXDkgpygPhK57JELT22QmbYEw7sq9DnpRuSswcSiF4',
     host: 'aws-us-east-1-portal.12.dblayer.com',
     port: 10419,
+    ssl: {
+      ca: caCert,
+    },
   },
   koa: {
     port: 3000,
--- database/thinkylocal.js
+++ database/thinkylocal.js
@@ -2,6 +2,9 @@
 module multiple times or else the instance of 'thinky' will not share the
 same models.  They recommend using this architecture to solve this issue. */
 
+
+const caCert = fs.readFileSync(__dirname + '/public-key.crt').toString().trim();
+
 const config = require('./config');
 
 const thinky = require('thinky');
